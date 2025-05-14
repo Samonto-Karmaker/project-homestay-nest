bcf77c069f29ca1d9eaca400126f6b3759c52ef5
Samonto Karmaker <samonto0726@gmail.com>
2025-05-14 19:27:52 +0600

    feat(common): add Joi validation for configuration

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -1,12 +1,18 @@
 import { Module } from "@nestjs/common"
-import { ConfigModule as NestConfigModule } from "@nestjs/config"
+import { ConfigService, ConfigModule as NestConfigModule } from "@nestjs/config"
+import * as Joi from "joi"
 
 @Module({
     imports: [
         NestConfigModule.forRoot({
             isGlobal: true,
             envFilePath: [".env"],
+            validationSchema: Joi.object({
+                MONGODB_URI: Joi.string().required(),
+            }),
         }),
     ],
+    providers: [ConfigService],
+    exports: [ConfigService],
 })
 export class ConfigModule {}
