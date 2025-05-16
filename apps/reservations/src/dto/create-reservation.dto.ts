f4b69f52c4f42b39fab80573337c415cfc68cf7c
Samonto Karmaker <samonto0726@gmail.com>
2025-05-16 17:43:03 +0600

    feat(reservations): add validation decorators to CreateReservationDto properties

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,6 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { Type } from "class-transformer"
+import { IsDate, IsNotEmpty, IsString } from "class-validator"
+
 export class CreateReservationDto {
+    @IsDate()
+    @Type(() => Date)
     startDate: Date
+
+    @IsDate()
+    @Type(() => Date)
     endDate: Date
+
+    @IsNotEmpty()
+    @IsString()
     placeId: string
+
+    @IsNotEmpty()
+    @IsString()
     invoiceId: string
 }
