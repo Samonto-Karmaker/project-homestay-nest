9d1ff9536b9b5978251b37251b801487060ac493
Samonto Karmaker <samonto0726@gmail.com>
2025-05-16 13:11:08 +0600

    feat(reservations): define Reservation entity with schema and properties

--- apps/reservations/src/entities/reservation.entity.ts
+++ apps/reservations/src/entities/reservation.entity.ts
@@ -1,1 +1,12 @@
-export class Reservation {}
+import { AbstractDocument } from "@app/common"
+import { Schema } from "@nestjs/mongoose"
+
+@Schema({ versionKey: false })
+export class Reservation extends AbstractDocument {
+    startDate: Date
+    endDate: Date
+    userId: string
+    placeId: string
+    invoiceId: string
+    timestamp: Date
+}
