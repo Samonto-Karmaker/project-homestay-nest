c7f8673a12d464296b74061b5f824abcadcf4fb1
Samonto Karmaker <samonto0726@gmail.com>
2025-05-16 12:16:04 +0600

    feat(common): add findOneAndUpdate and findOneAndDelete methods to AbstractRepository

--- libs/common/src/db/abstract.repository.ts
+++ libs/common/src/db/abstract.repository.ts
@@ -1,6 +1,6 @@
 import { Logger, NotFoundException } from "@nestjs/common"
 import { AbstractDocument } from "./abstract.schema"
-import { FilterQuery, Model, Types } from "mongoose"
+import { FilterQuery, Model, Types, UpdateQuery } from "mongoose"
 
 export abstract class AbstractRepository<
     DocumentType extends AbstractDocument,
@@ -9,6 +9,7 @@ export abstract class AbstractRepository<
 
     constructor(protected readonly model: Model<DocumentType>) {}
 
+    // Create
     async create(document: Omit<DocumentType, "_id">): Promise<DocumentType> {
         const createdDocument = new this.model({
             ...document,
@@ -21,6 +22,7 @@ export abstract class AbstractRepository<
         ).toJSON() as unknown as DocumentType
     }
 
+    // Read
     async findOne(filter: FilterQuery<DocumentType>): Promise<DocumentType> {
         const document = await this.model
             .findOne(filter)
@@ -36,6 +38,7 @@ export abstract class AbstractRepository<
         return document
     }
 
+    // Read one
     async find(): Promise<DocumentType[]> {
         const documents = await this.model.find().lean<DocumentType[]>(true)
         if (!documents) {
@@ -43,5 +46,46 @@ export abstract class AbstractRepository<
             throw new NotFoundException(`No documents found`)
         }
         return documents
+    }
+
+    // Update
+    async findOneAndUpdate(
+        filter: FilterQuery<DocumentType>,
+        update: UpdateQuery<DocumentType>
+    ): Promise<DocumentType> {
+        const document = await this.model
+            .findOneAndUpdate(filter, update, {
+                new: true,
+            })
+            .lean<DocumentType>(true)
+
+        if (!document) {
+            this.logger.error(
+                `Document not found with filter: ${JSON.stringify(filter)}`
+            )
+            throw new NotFoundException(
+                `Document not found with filter: ${JSON.stringify(filter)}`
+            )
+        }
+        return document
+    }
+
+    // Delete
+    async findOneAndDelete(
+        filter: FilterQuery<DocumentType>
+    ): Promise<DocumentType> {
+        const document = await this.model
+            .findOneAndDelete(filter)
+            .lean<DocumentType>(true)
+
+        if (!document) {
+            this.logger.error(
+                `Document not found with filter: ${JSON.stringify(filter)}`
+            )
+            throw new NotFoundException(
+                `Document not found with filter: ${JSON.stringify(filter)}`
+            )
+        }
+        return document
     }
 }
