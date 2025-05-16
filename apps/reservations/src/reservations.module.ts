08a1d5a0c077d706f40e8f95658503e492158e9c
Samonto Karmaker <samonto0726@gmail.com>
2025-05-16 12:55:54 +0600

    feat(reservations): initialize CRUD operations and DTOs for reservations management

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,6 +1,6 @@
 import { Module } from "@nestjs/common"
-import { ReservationsController } from "./reservations.controller"
 import { ReservationsService } from "./reservations.service"
+import { ReservationsController } from "./reservations.controller"
 import { DbModule } from "@app/common"
 
 @Module({
