6c42e0f233874711545ca4ae891958701b189f47
Samonto Karmaker <samonto0726@gmail.com>
2025-05-16 10:56:18 +0600

    feat(common): refactor AbstractRepository and AbstractDocument for improved system design

--- libs/common/src/db/abstract.repository.ts
+++ libs/common/src/db/abstract.repository.ts
@@ -2,18 +2,18 @@ import { Logger } from "@nestjs/common"
 import { AbstractDocument } from "./abstract.schema"
 import { Model, Types } from "mongoose"
 
-export abstract class AbstractRepository<T> extends AbstractDocument {
+export abstract class AbstractRepository<
+    DocumentType extends AbstractDocument,
+> {
     protected abstract readonly logger: Logger
 
-    constructor(protected readonly model: Model<T>) {
-        super()
-    }
+    constructor(protected readonly model: Model<DocumentType>) {}
 
-    async create(document: Omit<T, "_id">): Promise<T> {
+    async create(document: Omit<DocumentType, "_id">): Promise<DocumentType> {
         const createdDocument = new this.model({
             ...document,
             _id: new Types.ObjectId(),
         })
-        return (await createdDocument.save()).toJSON() as T
+        return (await createdDocument.save()).toJSON() as DocumentType
     }
 }
