8c99d2696f81137fc7fb254cb02acf916796b879
Samonto Karmaker <samonto0726@gmail.com>
2025-05-16 13:46:48 +0600

    feat(reservations): add Prop decorators to Reservation entity properties

--- apps/reservations/src/entities/reservation.entity.ts
+++ apps/reservations/src/entities/reservation.entity.ts
@@ -1,12 +1,25 @@
 import { AbstractDocument } from "@app/common"
-import { Schema } from "@nestjs/mongoose"
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose"
 
 @Schema({ versionKey: false })
 export class Reservation extends AbstractDocument {
+    @Prop()
     startDate: Date
+
+    @Prop()
     endDate: Date
+
+    @Prop()
     userId: string
+
+    @Prop()
     placeId: string
+
+    @Prop()
     invoiceId: string
+
+    @Prop()
     timestamp: Date
 }
+
+export const ReservationsSchema = SchemaFactory.createForClass(Reservation)
