de9547be968e934dd3cff845f379e53b29a5527c
Samonto Karmaker <samonto0726@gmail.com>
2025-05-16 12:44:01 +0600

    refactor: remove unused App module and related files; update nest-cli configuration for reservations module

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common"
 import { ReservationsController } from "./reservations.controller"
 import { ReservationsService } from "./reservations.service"
+import { DbModule } from "@app/common"
 
 @Module({
-    imports: [],
+    imports: [DbModule],
     controllers: [ReservationsController],
     providers: [ReservationsService],
 })
