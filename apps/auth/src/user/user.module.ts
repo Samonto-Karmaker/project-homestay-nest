5119487467dfe6520366f014b7557b52a44434e0
Samonto Karmaker <samonto0726@gmail.com>
2025-05-29 17:48:09 +0600

    feat(user): update UserModule to include UserRepository and LoggerModule

--- apps/auth/src/user/user.module.ts
+++ apps/auth/src/user/user.module.ts
@@ -1,9 +1,18 @@
 import { Module } from "@nestjs/common"
 import { UserController } from "./user.controller"
 import { UserService } from "./user.service"
+import { DbModule } from "@app/common"
+import { User, UserSchema } from "../entities/user.entity"
+import { LoggerModule } from "nestjs-pino"
+import { UserRepository } from "./user.repository"
 
 @Module({
+    imports: [
+        DbModule,
+        DbModule.forFeature([{ name: User.name, schema: UserSchema }]),
+        LoggerModule,
+    ],
     controllers: [UserController],
-    providers: [UserService],
+    providers: [UserService, UserRepository],
 })
 export class UserModule {}
