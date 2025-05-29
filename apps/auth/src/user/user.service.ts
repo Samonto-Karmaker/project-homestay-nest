ca8fba3c7fbd58c564135e8587144bc1e0c9b814
Samonto Karmaker <samonto0726@gmail.com>
2025-05-29 17:53:52 +0600

    feat(user): implement UserService with create method for user registration

--- apps/auth/src/user/user.service.ts
+++ apps/auth/src/user/user.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from "@nestjs/common"
+import { UserRepository } from "./user.repository"
+import { CreateUserDto } from "./dto/create-user.dto"
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private readonly userRepository: UserRepository) {}
+
+    create(createUserDto: CreateUserDto) {
+        return this.userRepository.create(createUserDto)
+    }
+}
