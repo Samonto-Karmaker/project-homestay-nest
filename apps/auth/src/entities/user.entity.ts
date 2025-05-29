de78822a9a6323ba24b406703a7225c560460033
Samonto Karmaker <samonto0726@gmail.com>
2025-05-29 17:28:19 +0600

    feat(user): implement User entity with username, email, and password properties

---
+++ apps/auth/src/entities/user.entity.ts
@@ -0,0 +1,16 @@
+import { AbstractDocument } from "@app/common"
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose"
+
+@Schema({ versionKey: false })
+export class User extends AbstractDocument {
+    @Prop()
+    username: string
+
+    @Prop({ unique: true })
+    email: string
+
+    @Prop()
+    password: string
+}
+
+export const UserSchema = SchemaFactory.createForClass(User)
