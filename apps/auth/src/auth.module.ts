e477534ba070af66bbb96b62622d5d774e205d7b
Samonto Karmaker <samonto0726@gmail.com>
2025-06-04 14:11:53 +0600

    feat(auth): configure JwtModule with async registration for JWT secret and expiration

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -2,9 +2,22 @@ import { Module } from "@nestjs/common"
 import { AuthController } from "./auth.controller"
 import { AuthService } from "./auth.service"
 import { UserModule } from "./user/user.module"
+import { JwtModule } from "@nestjs/jwt"
+import { ConfigService } from "@nestjs/config"
 
 @Module({
-    imports: [UserModule],
+    imports: [
+        UserModule,
+        JwtModule.registerAsync({
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                secret: configService.get<string>("JWT_SECRET"),
+                signOptions: {
+                    expiresIn: `${configService.get("JWT_EXPIRATION")}s`,
+                },
+            }),
+        }),
+    ],
     controllers: [AuthController],
     providers: [AuthService],
 })
