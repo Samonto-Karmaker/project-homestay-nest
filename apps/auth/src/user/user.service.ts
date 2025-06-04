7cdf7ec1d1692921bae5be929c16c52f755a3a06
Samonto Karmaker <samonto0726@gmail.com>
2025-06-04 15:17:39 +0600

    feat(user): hash password before creating user in the database

--- apps/auth/src/user/user.service.ts
+++ apps/auth/src/user/user.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from "@nestjs/common"
 import { UserRepository } from "./user.repository"
 import { CreateUserDto } from "./dto/create-user.dto"
+import { hash } from "bcryptjs"
 
 @Injectable()
 export class UserService {
     constructor(private readonly userRepository: UserRepository) {}
 
-    create(createUserDto: CreateUserDto) {
-        return this.userRepository.create(createUserDto)
+    async create(createUserDto: CreateUserDto) {
+        return this.userRepository.create({
+            ...createUserDto,
+            password: await hash(createUserDto.password, 10),
+        })
     }
 }
