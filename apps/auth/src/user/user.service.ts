7cd5ee516df6ad012a9e0e9942f524444b96945c
Samonto Karmaker <samonto0726@gmail.com>
2025-06-04 15:25:11 +0600

    feat(auth): add user validation method to check credentials

--- apps/auth/src/user/user.service.ts
+++ apps/auth/src/user/user.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from "@nestjs/common"
+import { Injectable, UnauthorizedException } from "@nestjs/common"
 import { UserRepository } from "./user.repository"
 import { CreateUserDto } from "./dto/create-user.dto"
-import { hash } from "bcryptjs"
+import { compare, hash } from "bcryptjs"
 
 @Injectable()
 export class UserService {
@@ -12,5 +12,14 @@ export class UserService {
             ...createUserDto,
             password: await hash(createUserDto.password, 10),
         })
+    }
+
+    async validateUser(email: string, password: string) {
+        const user = await this.userRepository.findOne({ email }) // throws NotFoundException if user not found
+        const isValid = user && (await compare(password, user.password))
+        if (!isValid) {
+            throw new UnauthorizedException("Invalid credentials")
+        }
+        return user
     }
 }
