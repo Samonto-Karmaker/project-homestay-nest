4366477c770faafa09b53ef97da074a020469ac4
Samonto Karmaker <samonto0726@gmail.com>
2025-06-12 17:48:27 +0600

    feat(reservations): integrate auth service to enable authentication

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common"
 import { ReservationsService } from "./reservations.service"
 import { ReservationsController } from "./reservations.controller"
-import { DbModule, LoggerModule } from "@app/common"
+import { AUTH_SERVICE, DbModule, LoggerModule } from "@app/common"
 import { ReservationsRepository } from "./reservations.repository"
 import { Reservation, ReservationsSchema } from "./entities/reservation.entity"
-import { ConfigModule } from "@nestjs/config"
+import { ConfigModule, ConfigService } from "@nestjs/config"
 import * as Joi from "joi"
+import { ClientsModule, Transport } from "@nestjs/microservices"
 
 @Module({
     imports: [
@@ -20,8 +21,23 @@ import * as Joi from "joi"
             validationSchema: Joi.object({
                 MONGODB_URI: Joi.string().required(),
                 HTTP_PORT: Joi.number().default(3000), // Default port
+                AUTH_HOST: Joi.string().default("localhost"),
+                AUTH_PORT: Joi.number().default(3002),
             }),
         }),
+        ClientsModule.registerAsync([
+            {
+                name: AUTH_SERVICE,
+                useFactory: (configService: ConfigService) => ({
+                    transport: Transport.TCP,
+                    options: {
+                        host: configService.get<string>("AUTH_HOST"),
+                        port: configService.get<number>("AUTH_PORT"),
+                    },
+                }),
+                inject: [ConfigService],
+            },
+        ]),
     ],
     controllers: [ReservationsController],
     providers: [ReservationsService, ReservationsRepository],
