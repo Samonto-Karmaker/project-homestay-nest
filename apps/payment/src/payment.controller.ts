3ebb22dce8bb64b25d221778b56361ef5f8577ac
Samonto Karmaker <samonto0726@gmail.com>
2025-06-18 16:11:47 +0600

    feat(payment): implement createPaymentIntent method in PaymentController

--- apps/payment/src/payment.controller.ts
+++ apps/payment/src/payment.controller.ts
@@ -1,7 +1,14 @@
 import { Controller } from "@nestjs/common"
 import { PaymentService } from "./payment.service"
+import { CreateChargeDto } from "./dto/create-charge.dto"
+import { MessagePattern, Payload } from "@nestjs/microservices"
 
 @Controller()
 export class PaymentController {
     constructor(private readonly paymentService: PaymentService) {}
+
+    @MessagePattern("createPaymentIntent")
+    createPaymentIntent(@Payload() createChargeDto: CreateChargeDto) {
+        return this.paymentService.createPaymentIntent(createChargeDto)
+    }
 }
