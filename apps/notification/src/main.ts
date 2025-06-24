28a55b7e894b876b2ead232726cda699846483f7
Samonto Karmaker <samonto0726@gmail.com>
2025-06-24 11:49:39 +0600

    feat(notification): configure microservice and enable CORS in main application

--- apps/notification/src/main.ts
+++ apps/notification/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from "@nestjs/core"
 import { NotificationModule } from "./notification.module"
+import { ConfigService } from "@nestjs/config"
+import { Transport } from "@nestjs/microservices"
+import { Logger } from "nestjs-pino"
 
 async function bootstrap() {
     const app = await NestFactory.create(NotificationModule)
-    await app.listen(process.env.port ?? 3004)
+    const configService = app.get(ConfigService)
+    app.connectMicroservice({
+        transport: Transport.TCP,
+        options: {
+            host: "0.0.0.0",
+            port: configService.get<number>("PORT", 3004),
+        },
+    })
+    app.useLogger(app.get(Logger))
+    app.enableCors({
+        origin: "*", // For development only! In production, specify allowed origins
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+        credentials: true,
+    })
+    await app.startAllMicroservices()
 }
 bootstrap()
--- apps/notification/src/notification.module.ts
+++ apps/notification/src/notification.module.ts
@@ -1,9 +1,21 @@
 import { Module } from "@nestjs/common"
 import { NotificationController } from "./notification.controller"
 import { NotificationService } from "./notification.service"
+import { ConfigModule } from "@nestjs/config"
+import * as Joi from "joi"
+import { LoggerModule } from "@app/common"
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: "apps/notification/.env",
+            validationSchema: Joi.object({
+                PORT: Joi.number().default(3004), // Default port for notification service
+            }),
+        }),
+        LoggerModule,
+    ],
     controllers: [NotificationController],
     providers: [NotificationService],
 })
