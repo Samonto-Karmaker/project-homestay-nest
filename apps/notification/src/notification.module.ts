a6a6ba7b3858a372b4cb5e72a2e3ed75f91c1d49
Samonto Karmaker <samonto0726@gmail.com>
2025-06-26 15:42:26 +0600

    feat(notification): implement email notification functionality with nodemailer integration

--- apps/notification/src/notification.module.ts
+++ apps/notification/src/notification.module.ts
@@ -12,6 +12,11 @@ import { LoggerModule } from "@app/common"
             envFilePath: "apps/notification/.env",
             validationSchema: Joi.object({
                 PORT: Joi.number().default(3004), // Default port for notification service
+                EMAIL_HOST: Joi.string().required(),
+                EMAIL_PORT: Joi.number().default(587),
+                EMAIL_USER: Joi.string().required(),
+                EMAIL_PASS: Joi.string().required(),
+                EMAIL_FROM: Joi.string().required(),
             }),
         }),
         LoggerModule,
--- apps/notification/src/notification.service.ts
+++ apps/notification/src/notification.service.ts
@@ -1,11 +1,41 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable } from "@nestjs/common"
 import { NotifyEmailDto } from "./dto/notify-email.dto"
+import { ConfigService } from "@nestjs/config"
+import * as nodemailer from "nodemailer"
 
 @Injectable()
 export class NotificationService {
-    notifyEmail({ email, text }: NotifyEmailDto) {
-        // Here you would implement the logic to send an email.
-        // This is just a placeholder implementation.
+    private readonly transporter: nodemailer.Transporter
+
+    constructor(private readonly configService: ConfigService) {
+        this.transporter = nodemailer.createTransport({
+            host: this.configService.get<string>("EMAIL_HOST"),
+            port: this.configService.get<number>("EMAIL_PORT"),
+            secure: this.configService.get<boolean>("EMAIL_SECURE"), // true for 465, false for other ports
+            auth: {
+                user: this.configService.get<string>("EMAIL_USER"),
+                pass: this.configService.get<string>("EMAIL_PASS"),
+            },
+        })
+    }
+
+    async notifyEmail({ email, text }: NotifyEmailDto) {
         console.log(`Sending email to ${email} with text: ${text}`)
+        const mailOptions = {
+            from: this.configService.get<string>("EMAIL_FROM"),
+            to: email,
+            subject: "Notification",
+            text: text,
+        }
+        try {
+            const info = await this.transporter.sendMail(mailOptions)
+            console.log("Email sent successfully!")
+            console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info))
+        } catch (error) {
+            console.error("Error sending email:", error)
+            throw new Error("Failed to send email")
+        }
     }
 }
