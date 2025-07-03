421d225a65176c86e180e18697d08b73a5eaeecc
Samonto Karmaker <samonto0726@gmail.com>
2025-07-03 14:41:31 +0600

    feat(e2e): implement health check tests for reservations, auth, payment, and notification services

--- e2e/specs/healthcheck.e2e.spec.ts
+++ e2e/specs/healthcheck.e2e.spec.ts
@@ -1,5 +1,37 @@
-describe("Initial-Test", () => {
-    it("should be truthy", () => {
-        expect(true).toBeTruthy()
+import { ping } from "tcp-ping"
+
+describe("Health-Check", () => {
+    test("reservations", async () => {
+        const res = await fetch("http://reservations:3000")
+        expect(res.ok).toBeTruthy()
+    })
+    test("auth", async () => {
+        const res = await fetch("http://auth:3001")
+        expect(res.ok).toBeTruthy()
+    })
+
+    test("payment", done => {
+        ping(
+            {
+                address: "payments",
+                port: 3003,
+            },
+            err => {
+                if (err) fail()
+                else done()
+            }
+        )
+    })
+    test("notification", done => {
+        ping(
+            {
+                address: "notifications",
+                port: 3003,
+            },
+            err => {
+                if (err) fail()
+                else done()
+            }
+        )
     })
 })
