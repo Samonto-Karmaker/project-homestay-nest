79b16279f051a2cdcda11073a3a2a4372ed3d227
Samonto Karmaker <samonto0726@gmail.com>
2025-07-08 16:23:52 +0600

    chore(grpc): add explanation on why we can't use pipes in grpc

--- apps/payment/src/payment.controller.ts
+++ apps/payment/src/payment.controller.ts
@@ -11,6 +11,13 @@ import {
 export class PaymentController implements PaymentServiceController {
     constructor(private readonly paymentService: PaymentService) {}
 
+    /* 
+        * Important Note:
+        We can't use pipes in gRPC,
+        because gRPC uses protobuf for serialization,
+        and pipes are designed for HTTP requests.
+        So we need to handle validation manually.
+    */
     createPaymentIntent(
         createPaymentIntentDto: CreatePaymentIntentWithEmailDto
     ) {
