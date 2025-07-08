7c3550e5af75f37ec59ac397cf90aa80e995aef8
Samonto Karmaker <samonto0726@gmail.com>
2025-07-08 18:50:06 +0600

    feat(gateway): setting up api gateway to integrate reservations service

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,9 +1,30 @@
+import { IntrospectAndCompose } from "@apollo/gateway"
 import { LoggerModule } from "@app/common"
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from "@nestjs/apollo"
 import { Module } from "@nestjs/common"
-import { ConfigModule } from "@nestjs/config"
+import { ConfigModule, ConfigService } from "@nestjs/config"
+import { GraphQLModule } from "@nestjs/graphql"
 
 @Module({
     imports: [
+        GraphQLModule.forRootAsync<ApolloGatewayDriverConfig>({
+            driver: ApolloGatewayDriver,
+            useFactory: (configService: ConfigService) => ({
+                gateway: {
+                    supergraphSdl: new IntrospectAndCompose({
+                        subgraphs: [
+                            {
+                                name: "reservations",
+                                url: configService.get(
+                                    "RESERVATIONS_GRAPHQL_URL"
+                                ),
+                            },
+                        ],
+                    }),
+                },
+            }),
+            inject: [ConfigService],
+        }),
         ConfigModule.forRoot({
             isGlobal: true,
             envFilePath: "apps/gateway/.env",
